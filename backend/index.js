85f883db8feecde954c95b6a0c5fa726557f8075
Aboli Deshmukh <abolideshmukh672@gmail.com>
2023-07-31 18:48:03 +0530

    themes and newsletter added

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const connectToMongo = require("./db");
 const express = require('express');
 const bodyParser = require('body-parser')
 const cors = require("cors");
-
+const https = require("https");
 
 
 connectToMongo();
@@ -19,6 +19,55 @@ app.use('/api/notes', require('./routes/notes.js'));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+const apiKey = "45b3a79c9389f53d71fd12a91e9a3775-us9";
+const listId = "1ff85afe5b";
+
+app.post("/newsletter", (req, res)=> {      // from user to our website
+   
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+
+  const data = {
+      members: [
+          {
+              email_address: email,
+              status: "subscribed",
+              merge_fields: {
+                  FNAME: firstName,
+                  LNAME: lastName
+              }
+          }
+      ]
+  }
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us9.api.mailchimp.com/3.0/lists/"+listId;
+  const options = {
+      method: "POST",
+      auth: "aboli:"+apiKey,
+  }
+
+  const request = https.request(url, options, function(response){   // from our server to mailchimp
+      response.on("data", function(data){
+          console.log(JSON.parse(data));
+      })
+
+      if(response.statusCode === 200){
+        res.send("success")
+          // res.sendFile(__dirname + "/success.html");
+      }
+      else{
+        res.send("failure")
+          // res.sendFile(__dirname +"/failure.html");
+      }
+  })
+
+  request.write(jsonData);
+  request.end();
 })
 
 app.listen(port, () => {
